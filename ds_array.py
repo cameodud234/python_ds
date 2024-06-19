a9dd5b9a2a8003447034ead660e242be7fa0c525
sunniestAcorn00 <30845397+cameodud234@users.noreply.github.com>
2024-06-18 23:05:57 -0400

    New changes.
    Iteration and print now work as expected.

--- ds_array.py
+++ ds_array.py
@@ -12,11 +12,26 @@ class Array:
             self._max_size = size
             self._current_size = 0
             self._allocated_cell = [0] * self._max_size
+            self._iter_position = 0
         else:
             raise ValueError(f"Value: {size}, of type: {type(size)}, is not an int.")
 
     def get_array(self):
         return self._allocated_cell
+    
+    def __str__(self):
+        return self._allocated_cell.__str__()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._iter_position < self._max_size:
+            value = self._allocated_cell[self._iter_position]
+            self._iter_position += 1
+            return value
+        else:
+            raise StopIteration
     
     # def combine_array(self, arr: iter) -> Array:
     #     """
@@ -28,3 +43,7 @@ class Array:
     #     if isinstance(arr, Array):
     #         self._allocated_cell = self._allocated_cell + arr.get_array()
     #     elif isinstance()
+
+
+
+class ArrayIter
