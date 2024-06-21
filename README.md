04332cb3dd3e59a2d3ec5aad68142dbf001d74e3
sunniestAcorn00 <30845397+cameodud234@users.noreply.github.com>
2024-06-21 15:55:27 -0400

    Made the constructor more concise.

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 
 Writing data structures in python.
+
+Run tests in the main directory using:
+    > python -m unittest discover -s tests
--- src/ds_array.py
+++ src/ds_array.py
@@ -1,34 +1,32 @@
-from typing import Union
+from typing import Union, TypeVar
+import logging
 
 class Array:
 
-    def __init__(self, size: int, typeof: type = int) -> None:
+    T = TypeVar('T', int, float)
+    
+    def __init__(self, size: int, typeof: type[T] = int) -> None:
         """
         Creates an array with a set size.
         If items are added past the max size the 
         array will resize.
         """
-        if isinstance(size, int) and not isinstance(size, bool):
 
+        if isinstance(size, int) and not isinstance(size, bool):
             if size < 0:
                 raise ValueError(f"size: {size}, must be greater than or equal zero.")
+            if typeof != int and typeof != float:
+                raise ValueError(f"Invalid typeof: typeof={typeof}, must be int or float.")
             self._max_size = size
             self._current_size = 0
             self._typeof = typeof
             self._iter_position = 0
-
-            if isinstance(typeof, type):
-                if typeof == int:
-                    self._allocated_cell = [0] * self._max_size
-
-                elif typeof == float:
-                    self._allocated_cell = [0.0] * self._max_size
-                else:
-                    raise ValueError(f"typeof value: {typeof}, must be an int or float.")
-            else:
-                raise ValueError(f"typeof: {type(typeof)}, must be a type.")
+            self._allocated_cell = [self._typeof(0)] * self._max_size
         else:
-            raise ValueError(f"size: {size}, must be of type int.")
+            raise ValueError(f"Invalid size: size={size}, must be an int.")
+        
+    # def _constructor_check(self, size: int, typeof: type[T]) -> None:
+
 
     def get_array(self):
         return self._allocated_cell
@@ -44,7 +42,6 @@ class Array:
             
             else:
                 raise IndexError(f"index: {index}, is out of list index range.")
-
     
     def __str__(self):
         return self._allocated_cell.__str__()
