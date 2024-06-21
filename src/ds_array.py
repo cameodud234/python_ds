7973a0ef55a96c8831c147ef55f9daecc0833397
sunniestAcorn00 <30845397+cameodud234@users.noreply.github.com>
2024-06-21 14:16:16 -0400

    Added changes to Array Class

--- src/ds_array.py
+++ src/ds_array.py
@@ -1,7 +1,8 @@
+from typing import Union
 
 class Array:
 
-    def __init__(self, size: int, typeof: type) -> None:
+    def __init__(self, size: int, typeof: type = int) -> None:
         """
         Creates an array with a set size.
         If items are added past the max size the 
@@ -9,17 +10,41 @@ class Array:
         """
         if isinstance(size, int) and not isinstance(size, bool):
 
-            if size <= 0:
-                raise ValueError(f"Size: {size}, must be larger than zero")
+            if size < 0:
+                raise ValueError(f"size: {size}, must be greater than or equal zero.")
             self._max_size = size
             self._current_size = 0
-            self._allocated_cell = [0] * self._max_size
+            self._typeof = typeof
             self._iter_position = 0
+
+            if isinstance(typeof, type):
+                if typeof == int:
+                    self._allocated_cell = [0] * self._max_size
+
+                elif typeof == float:
+                    self._allocated_cell = [0.0] * self._max_size
+                else:
+                    raise ValueError(f"typeof value: {typeof}, must be an int or float.")
+            else:
+                raise ValueError(f"typeof: {type(typeof)}, must be a type.")
         else:
-            raise ValueError(f"Value: {size}, of type: {type(size)}, is not an int.")
+            raise ValueError(f"size: {size}, must be of type int.")
 
     def get_array(self):
         return self._allocated_cell
+    
+    def __getitem__(self, index: int):
+        if isinstance(index, int) and not isinstance(index, bool):
+
+            if self._current_size == 0:
+                raise IndexError(f"array: {self._allocated_cell} has no members to call upon.")
+            
+            if index < self._current_size and index > 0:
+                return self._allocated_cell[index]
+            
+            else:
+                raise IndexError(f"index: {index}, is out of list index range.")
+
     
     def __str__(self):
         return self._allocated_cell.__str__()
@@ -34,18 +59,3 @@ class Array:
             return value
         else:
             raise StopIteration
-    
-    # def combine_array(self, arr: iter) -> Array:
-    #     """
-    #     Adds values of 
-    #     """
-
-    #     accepted_types = [iter]
-
-    #     if isinstance(arr, Array):
-    #         self._allocated_cell = self._allocated_cell + arr.get_array()
-    #     elif isinstance()
-
-
-
-# class ArrayIter
